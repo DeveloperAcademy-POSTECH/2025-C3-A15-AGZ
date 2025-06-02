7372a316d07b3ea97b103469eb491bc722f39bd3
Seungjin Lee <sjin3687@gmail.com>
2025-06-02 16:49:53 +0900

    [#7] Design: CheerSongView 구현

---
+++ CheerLot/CheerLot/Resources/Presentation/CheerSong/CheerSongViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  CheerSongViewModel.swift
+//  CheerLot
+//
+//  Created by 이승진 on 6/1/25.
+//
+
+import AVFoundation
+import Foundation
+import Observation
+
+@Observable
+class CheerSongViewModel {
+
+}
