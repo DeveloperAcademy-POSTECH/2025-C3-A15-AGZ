d2318e4f5425694fb68ad3f3e4c1cd002ffae69e
dlguszoo <lhjivy1024@gmail.com>
2025-06-03 21:22:13 +0900

    [#23] Feat: StartingMemberListView 구현

---
+++ CheerLot/WatchCheerLot Watch App/Resources/Presentation/StartingMember/NextView.swift
@@ -0,0 +1,19 @@
+//
+//  NextView.swift
+//  WatchCheerLot Watch App
+//
+//  Created by 이현주 on 6/3/25.
+//
+
+import SwiftUI
+
+struct NextView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .navigationTitle("류지혁")
+    }
+}
+
+#Preview {
+    NextView()
+}
--- CheerLot/WatchCheerLot Watch App/Resources/Presentation/StartingMember/StartingMemberListView.swift
+++ CheerLot/WatchCheerLot Watch App/Resources/Presentation/StartingMember/StartingMemberListView.swift
@@ -8,11 +8,30 @@
 import SwiftUI
 
 struct StartingMemberListView: View {
-  var body: some View {
-    Text( /*@START_MENU_TOKEN@*/"Hello, World!" /*@END_MENU_TOKEN@*/)
-  }
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                Image(.ssListBG)
+                    .resizable()
+                    .ignoresSafeArea()
+                
+                List {
+                    ForEach(0...4, id: \.self) {
+                        member in
+                        NavigationLink {
+                            NextView()
+                        } label: {
+                            Text("1  류지혁")
+                                .font(.dynamicPretend(type: .semibold, size: 17))
+                                .padding(.leading, WatchDynamicLayout.dynamicValuebyWidth(10))
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 #Preview {
-  StartingMemberListView()
+    StartingMemberListView()
 }
