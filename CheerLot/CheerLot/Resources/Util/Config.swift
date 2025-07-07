6a1b9332a4257f433d562c7f8f5bbebbf8172356
dlguszoo <lhjivy1024@gmail.com>
2025-07-07 19:55:59 +0900

    [#64] Feat: API_URL Config 구현

--- CheerLot/CheerLot/Resources/Util/Config.swift
+++ CheerLot/CheerLot/Resources/Util/Config.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+enum Config {
+    private static let infoDictionary: [String : Any] = {
+        guard let dict = Bundle.main.infoDictionary else {
+            fatalError("Plist 없음")
+        }
+        return dict
+    }()
+    
+    static let apiURL: String = {
+        guard let apiURL = infoDictionary["API_URL"] as? String else {
+            fatalError()
+        }
+        return apiURL
+    }()
+}
