9b704cb2a0dc1cfcffffffaf70cd4ffc3e044cda
dlguszoo <lhjivy1024@gmail.com>
2025-07-11 04:09:42 +0900

    [#65] Fix: 개인 응원가 없는 Text 중앙정렬

--- CheerLot/WatchCheerLot Watch App/Resources/Presentation/Lyrics/LyricsView.swift
+++ CheerLot/WatchCheerLot Watch App/Resources/Presentation/Lyrics/LyricsView.swift
@@ -8,24 +8,24 @@
 import SwiftUI
 
 struct LyricsView: View {
-  let player: PlayerWatchDto
-
-  var body: some View {
-    ScrollView {
-      if !player.cheerSongList.isEmpty {
-        VStack(alignment: .leading, spacing: 24) {
-          ForEach(player.cheerSongList, id: \.self) { song in
-            Text(song.lyrics)
-              .font(.dynamicPretend(type: .bold, size: 24))
-          }
+    let player: PlayerWatchDto
+    
+    var body: some View {
+        if !player.cheerSongList.isEmpty {
+            ScrollView {
+                VStack(alignment: .leading, spacing: 24) {
+                    ForEach(player.cheerSongList, id: \.self) { song in
+                        Text(song.lyrics)
+                            .font(.dynamicPretend(type: .bold, size: 24))
+                    }
+                }
+                .navigationTitle(player.name)
+                .padding()
+            }
+        } else {
+            Text("아직 개인\n응원가가 없어요")
+                .font(.dynamicPretend(type: .bold, size: 18))
+                .multilineTextAlignment(.center)
         }
-        .navigationTitle(player.name)
-        .padding()
-      } else {
-        Text("아직 개인\n응원가가 없어요")
-          .font(.dynamicPretend(type: .bold, size: 18))
-          .multilineTextAlignment(.center)
-      }
     }
-  }
 }
